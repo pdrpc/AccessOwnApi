cf61247899b409378bc4657d743ef1ab88da7f46
pdrpc <pdr.pradocamargo@gmail.com>
2021-04-14 16:35:54 -0300

    Adicionar arquivos de projeto.

---
+++ AccessOwnApi/Controllers/AddressController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccessOwnApi.Controllers
+{
+    public class AddressController : Controller
+    {
+        // GET: Address
+        public ActionResult Index()
+        {
+            //Requisição com lista endpoint api/address
+            return View();
+        }
+
+        public ActionResult GetById() 
+        {
+            //Requisição GET com Id api/address/id
+            return View();
+        }
+
+        public ActionResult RegisterNewAddress() 
+        {
+            //requisição POST com o cep api/address
+            return View();
+        }
+
+        public ActionResult ModifyAddress() {
+            //requisição PUT com o Address no Body
+            return View();
+        }
+
+        public ActionResult DeleteAddress() {
+            //Requisição DELETE com o Id
+            return View();
+        }
+    }
+}
---
+++ AccessOwnApi/Models/Address.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccessOwnApi.Models
+{
+    public class Address
+    {
+        [JsonProperty("cep")]
+        public string Cep { get; set; }
+
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+
+        [JsonProperty("complemento")]
+        public string Complemento { get; set; }
+
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+
+        [JsonProperty("localidade")]
+        public string Localidade { get; set; }
+
+        [JsonProperty("uf")]
+        public string Uf { get; set; }
+
+        [JsonProperty("ibge")]
+        public long Ibge { get; set; }
+
+        [JsonProperty("gia")]
+        public long Gia { get; set; }
+
+        [JsonProperty("ddd")]
+        public long Ddd { get; set; }
+
+        [JsonProperty("siafi")]
+        public long Siafi { get; set; }
+    }
+}
